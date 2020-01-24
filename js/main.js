6563503d2840830830a2e8d8bb71d8bac818e873
Baron Pollak <pollakkina@outlook.com>
2020-01-23 22:14:09 -0500

    adding in UI and Redoing HomePage

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 let money = 0;
+let power = 0;
+
 let repair = 0;
 let robot = 0;
 
 let prestige = 0;
-
-let health = 0;
-let maxHealth = 100;
 
 function clickerClick(number){
     money = money + number;
@@ -18,12 +17,13 @@ function prettify(input){
     return output.toString();
 }
 
+
+
 /**
  * All of the upgrades go here, so any of the ability upgrades
  */
 
-
-function buyRobot(){
+function buyRobot(id){
     let robotCost = Math.floor(10 * Math.pow(1.1, robot));
     if(money >= robotCost){
         robot = robot + 1;
@@ -38,18 +38,7 @@ function buyRobot(){
 
 }
 
-/*
-function buyCursor(){
-    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
-    if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
-        cursors = cursors + 1;                                   //increases number of cursors
-    	cookies = cookies - cursorCost;                          //removes the cookies spent
-        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
-        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
-    };
-    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
-    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
-};*/
+
 
 
 /**
